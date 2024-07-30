13ca4c87ed399036411181e4920876a774d17386
EtherXion <109169177+EtherXion@users.noreply.github.com>
2024-07-30 21:12:18 +0900

    Add files via upload

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.Queue;
 import java.util.Scanner;
 
 public class App {
@@ -23,13 +24,16 @@ public class App {
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             if (sc.next().equals("remove")) {
-                calculator.queue.remove();
+                Queue<Integer> queue = calculator.getQueue();
+                queue.remove();
+                calculator.setQueue(queue);
             }
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             if (sc.next().equals("inquiry")) {
+                Queue<Integer> queue = calculator.getQueue();
                 // forEach 사용
-                for (Integer que : calculator.queue) {
+                for (Integer que : queue) {
                     System.out.println(que);
                 }
             }
--- Calculator.java
+++ Calculator.java
@@ -6,7 +6,16 @@ import java.util.Queue;
 public class Calculator {
     /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
 
-    Queue<Integer> queue = new LinkedList<>();
+
+    private Queue<Integer> queue = new LinkedList<>();
+
+    public Queue<Integer> getQueue() {
+        return new LinkedList<>(queue); // 그냥 return queue 보다 안전?
+    }
+    public void setQueue(Queue<Integer> queue) {
+        this.queue = new LinkedList<>(queue);
+    }
+
 
     int result = 0;
 
