711be2664c416d4e6c6abb828bd0d2b2fa0c618c
EtherXion <109169177+EtherXion@users.noreply.github.com>
2024-07-30 17:49:38 +0900

    Add files via upload

--- App.java
+++ App.java
@@ -1,57 +1,38 @@
 package calculator;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
 
-        Queue<Integer> queue = new LinkedList<>();
-
         Scanner sc = new Scanner(System.in);
 
-        int result = 0;
+        Calculator calculator = new Calculator();
 
         do {
 
             System.out.println("첫 번째 숫자 입력 : ");
             int FirstNumber = sc.nextInt();
-            System.out.println("첫 번째 숫자 입력 : ");
+            System.out.println("두 번째 숫자 입력 : ");
             int SecondNumber = sc.nextInt();
             System.out.println("사칙연산 기호 입력 : ");
             String A = sc.next();
 
-            if (A.equals("+")) {
-                result = FirstNumber + SecondNumber;
-                queue.add(result);
-            } else if (A.equals("-")) {
-                result = FirstNumber - SecondNumber;
-                queue.add(result);
-            } else if (A.equals("*")) {
-                result = FirstNumber * SecondNumber;
-                queue.add(result);
-            } else if (A.equals("/")) {
-                result = FirstNumber / SecondNumber;
-                queue.add(result);
-            }
-
-            System.out.println("결과 : " + result);
+            System.out.println("결과 : " + calculator.calculate(FirstNumber,SecondNumber,A));
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             if (sc.next().equals("remove")) {
-                queue.remove();
+                calculator.queue.remove();
             }
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             if (sc.next().equals("inquiry")) {
                 // forEach 사용
-                for (Integer que : queue) {
+                for (Integer que : calculator.queue) {
                     System.out.println(que);
                 }
             }
-
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) : ");
         } while (!sc.next().equals("exit"));
--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,39 @@
 package calculator;
 
-public class Calculator {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Calculator {
+    /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
+
+    Queue<Integer> queue = new LinkedList<>();
+
+    int result = 0;
+
+    public int calculate(int a, int b , String c) {
+        /* 위 요구사항에 맞게 구현 */
+        /* return 연산 결과 */
+
+        if (c.equals("+")) {
+            result = a + b;
+            queue.add(result);
+        } else if (c.equals("-")) {
+            result = a - b;
+            queue.add(result);
+        } else if (c.equals("*")) {
+            result = a * b;
+            queue.add(result);
+        } else if (c.equals("/")) {
+            if (b == 0) {
+                throw new ArithmeticException();
+            } else {
+                result = a / b;
+                queue.add(result);
+            }
+        } else {
+            throw new ArithmeticException();
+        }
+
+        return result;
+    }
 }
