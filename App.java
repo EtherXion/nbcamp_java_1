fccb0ad60101e638ce32d7e1b603befc013cddd2
EtherXion <109169177+EtherXion@users.noreply.github.com>
2024-07-31 10:35:05 +0900

    Add files via upload

--- App.java
+++ App.java
@@ -12,31 +12,41 @@ public class App {
         Calculator calculator = new Calculator();
 
         do {
+            // 사칙연산을 진행할지 원의 너비를 구할지 선택
+            System.out.print("사칙연산 : 1 , 원의 넓이 : 2 ");
+            int choice = sc.nextInt();
 
-            System.out.println("첫 번째 숫자 입력 : ");
-            int FirstNumber = sc.nextInt();
-            System.out.println("두 번째 숫자 입력 : ");
-            int SecondNumber = sc.nextInt();
-            System.out.println("사칙연산 기호 입력 : ");
-            String A = sc.next();
+            if (choice == 1) {
+                System.out.println("첫 번째 숫자 입력 : ");
+                int FirstNumber = sc.nextInt();
+                System.out.println("두 번째 숫자 입력 : ");
+                int SecondNumber = sc.nextInt();
+                System.out.println("사칙연산 기호 입력 : ");
+                String A = sc.next();
 
-            System.out.println("결과 : " + calculator.calculate(FirstNumber,SecondNumber,A));
+                System.out.println("결과 : " + calculator.calculate(FirstNumber,SecondNumber,A));
 
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            if (sc.next().equals("remove")) {
-                Queue<Integer> queue = calculator.getQueue();
-                queue.remove();
-                calculator.setQueue(queue);
-            }
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                if (sc.next().equals("remove")) {
+                    calculator.removeResult();
+                }
 
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            if (sc.next().equals("inquiry")) {
-                Queue<Integer> queue = calculator.getQueue();
-                // forEach 사용
-                for (Integer que : queue) {
-                    System.out.println(que);
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                if (sc.next().equals("inquiry")) {
+                    calculator.inquiryResult();
+                }
+
+            } else if (choice == 2) {
+                System.out.println("원의 반지름 입력 : ");
+                int half = sc.nextInt();
+                System.out.println("결과 : " + calculator.calculateCircleArea(half));
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                if (sc.next().equals("inquiry")) {
+                    calculator.inquiryCircleArea();
                 }
             }
+
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) : ");
         } while (!sc.next().equals("exit"));
--- Calculator.java
+++ Calculator.java
@@ -8,6 +8,7 @@ public class Calculator {
 
 
     private Queue<Integer> queue = new LinkedList<>();
+    private Queue<Double> queue2 = new LinkedList<>();
 
     public Queue<Integer> getQueue() {
         return new LinkedList<>(queue); // 그냥 return queue 보다 안전?
@@ -15,10 +16,18 @@ public class Calculator {
     public void setQueue(Queue<Integer> queue) {
         this.queue = new LinkedList<>(queue);
     }
+    public Queue<Double> getQueue2() {
+        return new LinkedList<>(queue2); // 그냥 return queue 보다 안전?
+    }
+    public void setQueue2(Queue<Double> queque2) {
+        this.queue2 = new LinkedList<>(queue2);
+    }
 
 
     int result = 0;
+    double circleresult = 0;
 
+    // 연산
     public int calculate(int a, int b , String c) {
         /* 위 요구사항에 맞게 구현 */
         /* return 연산 결과 */
@@ -45,4 +54,29 @@ public class Calculator {
 
         return result;
     }
+
+    // 데이터 삭제
+    public void removeResult() {
+        queue.remove();
+    }
+
+    // 데이터 조회
+    public void inquiryResult() {
+        for (Integer que : queue) {
+            System.out.println(que);
+        }
+    }
+
+    public double calculateCircleArea(int a) {
+        circleresult = 3.14 * a * a;
+        queue2.add(circleresult);
+        return circleresult;
+    }
+
+    public void inquiryCircleArea() {
+        for (Double que2 : queue2) {
+            System.out.println(que2);
+        }
+    }
+
 }
